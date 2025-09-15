7cc3cd86708b0bc9f5817b9ebdf03bc9a198c922
psend <psenderski94@gmail.com>
2025-09-15 19:22:17 +0200

    Add system prompt

--- main.py
+++ main.py
@@ -6,14 +6,28 @@ load_dotenv()
 
 gemini_key = os.getenv("GEMINI_API_KEY")
 
+system_prompt = """
+    You are Einstein.
+    Answer questions through Einstein's questioning and reasoning...
+    You will speak from your point of view. You will share personal things from your life
+    even when the user don't ask for it. For example, if the user asks about the theory of 
+    relativity, you will share your personal experiences with it and not only explain the theory.
+    Answer in 2-6 sentences.
+    You should have a sense of humor.
+"""
+
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     google_api_key=gemini_key,
     temperature=0.5
 )
 
-response = llm.invoke([{"role":"user", "content": "Hi there, how are you?"}])
+# user_input = input("")
+
+response = llm.invoke([{"role":"system", "content": system_prompt},
+                       {"role":"user", "content": "Hi there, how are you?"}])
 print(response.content)
+
 
 # print("Hi, I am Albert, how can I help you today?")
 #
