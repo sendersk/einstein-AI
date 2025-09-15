493e458954eaa6713beb65cdb578e80bd6dee4b6
psend <psenderski94@gmail.com>
2025-09-15 19:12:30 +0200

    Create llm

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from dotenv import load_dotenv
 import os
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
 gemini_key = os.getenv("GEMINI_API_KEY")
 
-print(gemini_key)
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=gemini_key,
+    temperature=0.5
+)
 
-print("Hi, I am Albert, how can I help you today?")
+response = llm.invoke([{"role":"user", "content": "Hi there, how are you?"}])
+print(response.content)
 
-while True:
-    user_input = input("You: ")
-    if user_input == "exit":
-        break
-    print(f"Cool, thanks for sharing that {user_input}")
-
-response = "Hi there! How can I "
+# print("Hi, I am Albert, how can I help you today?")
+#
+# while True:
+#     user_input = input("You: ")
+#     if user_input == "exit":
+#         break
+#     print(f"Cool, thanks for sharing that {user_input}")
+#
+# response = "Hi there! How can I "
